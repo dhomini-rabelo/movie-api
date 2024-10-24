1fe79d474f85ede86fee45ae7e71f08552ef107b
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2024-10-24 10:34:54 -0300

    feat[UpdateUserUseCase]: it should update a user

---
+++ server/src/domain/bounded-contexts/auth/application/use-cases/update/update.spec.ts
@@ -0,0 +1,30 @@
+import { UserFactory } from '../../__tests__/factories/user'
+import { InMemoryUserRepository } from '../../__tests__/repositories/user'
+import { User } from '../../../enterprise/entities/user'
+import { UpdateUserUseCase } from './update'
+
+describe('UpdateUserUseCase', () => {
+  const userRepository = new InMemoryUserRepository()
+  const userFactory = new UserFactory(userRepository)
+  const sut = new UpdateUserUseCase(userRepository)
+
+  beforeEach(async () => {
+    await userRepository.reset()
+  })
+
+  it('should update a user', async () => {
+    const user = await userFactory.create()
+    const newUsername = 'new-username'
+
+    const response = await sut.execute({
+      id: user.id.toString(),
+      data: {
+        username: newUsername,
+      },
+    })
+
+    expect(response).instanceOf(User)
+    expect(response.props.username).toBe(newUsername)
+    expect(user.props.username).not.toBe(response.props.username)
+  })
+})
---
+++ server/src/domain/bounded-contexts/auth/application/use-cases/update/update.ts
@@ -0,0 +1,19 @@
+import { UseCase } from '@/domain/core/use-cases/base'
+import { createID } from '@tests/utils/domain'
+
+import { UserRepository } from '../../repositories/user'
+
+interface Payload {
+  id: string
+  data: {
+    username: string
+  }
+}
+
+export class UpdateUserUseCase implements UseCase {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async execute(payload: Payload) {
+    return this.userRepository.update(createID(payload.id), payload.data)
+  }
+}
