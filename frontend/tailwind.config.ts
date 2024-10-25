8bca4216876b746b1e7fc8e3ab8fe6343693827c
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2024-10-25 17:26:26 -0300

    start

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -9,8 +9,32 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        Black: {
+          100: '#313133',
+          200: '#202124',
+          300: '#1D1D1F',
+          400: '#171719',
+          500: '#040B14',
+        },
+        Gray: {
+          700: '#8C8A93',
+          500: '#A5A8B0',
+        },
+        Green: {
+          300: '#45EA69',
+          400: '#3bd25c',
+        },
+        Red: {
+          400: '#FF6363',
+          500: '#Ef4444',
+          600: '#f23232',
+        },
+        Purple: {
+          400: '#2C1C4B',
+        },
+        White: {
+          300: '#DCDEE3',
+        },
       },
     },
   },
